695e692b0ec82b3ee2e23bde679a0f88d0a77a98
Alesha <alesha.eble@gmail.com>
2022-04-22 20:57:33 -0500

    Starting to move common functionality to sheets_client.py

--- ProphetBot/sheets_client.py
+++ ProphetBot/sheets_client.py
@@ -3,11 +3,13 @@ import gspread
 import json
 import os
 from time import perf_counter
+from typing import List, Optional
 from ProphetBot.constants import TIERS, SHOP_TIERS
+from ProphetBot.models.sheets_objects import Character
 
 
 class GsheetsClient(object):
-    
+
     def __init__(self):
         # Initial _auth
         start = perf_counter()
@@ -44,3 +46,19 @@ class GsheetsClient(object):
 
     def get_shop_tier(self) -> int:
         return bisect.bisect(SHOP_TIERS, self.get_asl())
+
+    def get_all_characters(self) -> List | List[Character]:
+        character_dicts = self.char_sheet.get_all_records(head=2)
+        characters = [Character(c) for c in character_dicts]
+        return characters
+
+    def get_character_from_id(self, discord_id: str) -> Optional[Character]:
+        header_row = '2:2'
+        target_cell = self.char_sheet.find(discord_id, in_column=1)
+        if not target_cell:
+            return None
+
+        user_row = str(target_cell.row) + ':' + str(target_cell.row)
+        data = self.char_sheet.batch_get([header_row, user_row])
+
+        return Character(data)
