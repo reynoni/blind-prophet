76fc6fc2a36ff6f9bfbd56550b5516bf112c637a
Alesha <alesha.eble@gmail.com>
2022-05-02 21:26:47 -0500

    Making it so we check against null() correctly in end_ts

--- ProphetBot/queries.py
+++ ProphetBot/queries.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.selectable import FromClause, TableClause
 
 def select_active_arena_by_channel(channel_id: int) -> FromClause:
     return arenas_table.select().where(
-        and_(arenas_table.c.channel_id == channel_id, arenas_table.c.end_ts is null)
+        and_(arenas_table.c.channel_id == channel_id, arenas_table.c.end_ts == null())
     )
 
 
